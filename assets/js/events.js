4c1c6f9db17793a987129f52f5ed29604f2beaed
Darklg <kev.rocher@gmail.com>
2014-02-28 23:51:13 +0100

    Clean & Reindent HTML + utiities pad & trim

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,10 +1,12 @@
+var htmlliz = false;
+
 window.domReady(function() {
+    htmlliz = new HTMLLisible();
     // On form submit
     $_('main-form').addEvent('submit', function(e) {
         window.eventPreventDefault(e);
         var contentItem = $_('content'),
-            htmlToClean = contentItem.value,
-            htmlliz = new HTMLLisible();
+            htmlToClean = contentItem.value;
         // Replace textarea value with clean HTML
         contentItem.value = htmlliz.clean(htmlToClean);
     });
--- assets/js/htmllisible.js
+++ assets/js/htmllisible.js
@@ -6,9 +6,10 @@ var HTMLLisible = function() {
     this.clean = function(html) {
         // Add missing slashes to autoclosing tags
         html = this.addMissingSlashes(html);
-        // Serialize HTML
-        html = this.serializeHTML(html);
-        // Reindent HTML
+        // Clean HTML
+        html = this.cleanHTML(html);
+        // Prepare indent HTML
+        html = this.reindentHTML(html);
         return html;
     };
 
@@ -17,16 +18,65 @@ var HTMLLisible = function() {
         return html;
     };
 
-    this.serializeHTML = function(html) {
-        html = String(html).replace(/>([^<]*)</g, '><');
+    this.cleanHTML = function(html) {
+        // Replace spaces after tag
+        html = String(html).replace(/>([ \t\n]+)/gm, '>');
+        // Replace spaces before tag
+        html = html.replace(/(\s\t\n+)</gm, '<');
+        // Set one tag per line
+        html = html.replace(/>([^<]*)</g, ">\n$1\n<");
         return html;
     };
 
     this.reindentHTML = function(html) {
-        // Set one tag per line
-        html = String(html).replace(/>([^<]*)</g, ">\n$2\n<");
-        // Merge double line breaks
-        html = String(html).replace(/\n\n/g, "\n");
-        return html;
+
+        // Extract lines
+        var lines = String(html).split("\n"),
+            linesLength = lines.length,
+            reindentedHTML = '',
+            isOpening = false,
+            indentLevel = 0,
+            indentHTML = '',
+            line = '';
+
+        // For each line
+        for (var i = 0; i < linesLength; i++) {
+
+            line = this.trim(lines[i]);
+            // Dont look at empty lines
+            if (!line) {
+                continue;
+            }
+
+            // Closing line
+            if (line.match(/^<\//)) {
+                indentLevel--;
+            }
+
+            // Prepare indentHTML
+            indentHTML = this.pad(indentLevel);
+
+            // Opening line (not unique tag)
+            if (line.match(/^<([a-zA-Z])/) && !line.match(/\/>$/)) {
+                indentLevel++;
+            }
+            reindentedHTML += indentHTML + line + "\n";
+        }
+
+        return reindentedHTML;
+    };
+
+    /* Utilities */
+
+    // Pad a string to a given number
+    this.pad = function(nb, str) {
+        str = !str ? "\t" : str;
+        nb = !nb ? 0 : nb;
+        return new Array(nb + 1).join(str);
+    };
+
+    // Trim a string
+    this.trim = function(str) {
+        return str.replace(/^\s+/g, '').replace(/\s+$/g, '');
     };
 };

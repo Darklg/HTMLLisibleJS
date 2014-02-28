8bde7a9b765886ab37a7ad248fd9544c1416e262
Darklg <kev.rocher@gmail.com>
2014-02-28 08:49:19 +0100

    Missing slashes function & start for reindent HTML function

--- assets/js/htmllisible.js
+++ assets/js/htmllisible.js
@@ -8,15 +8,25 @@ var HTMLLisible = function() {
         html = this.addMissingSlashes(html);
         // Serialize HTML
         html = this.serializeHTML(html);
+        // Reindent HTML
         return html;
     };
 
     this.addMissingSlashes = function(html) {
+        html = String(html).replace(/<(meta|br|hr|link)([^>\/]*)>/g, '<$1$2 />');
         return html;
     };
 
     this.serializeHTML = function(html) {
         html = String(html).replace(/>([^<]*)</g, '><');
+        return html;
+    };
+
+    this.reindentHTML = function(html) {
+        // Set one tag per line
+        html = String(html).replace(/>([^<]*)</g, ">\n$2\n<");
+        // Merge double line breaks
+        html = String(html).replace(/\n\n/g, "\n");
         return html;
     };
 };
